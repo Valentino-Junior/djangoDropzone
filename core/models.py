057bdb7c10262d62865fc6d86e30eb980e9c92e2
meshack34 <meshackkimutai34@gail.com>
2022-09-30 11:25:20 +0300

    upload mulitiple images

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Image(models.Model):
+    image=models.ImageField(upload_to='images/')
+    date = models.DateTimeField( auto_now_add=True)
+
+    class Meta:
+        ordering=['-date']
+
+    def __str__(self):
+        return str(self.date)
---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Image
+# Create your views here.
+
+def home(request):
+    images=Image.objects.all()
+    context={
+        'images':images
+    }
+    return render(request, 'index.html', context)
+
+def file_upload(request):
+    if request.method == 'POST':
+        my_file=request.FILES.get('file')
+        Image.objects.create(image=my_file)
+        return HttpResponse('')
+    return JsonResponse({'post':'fasle'})
