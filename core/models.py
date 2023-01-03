6ecdd0d80075e1c37e77b9fd8014b8eabfa1d1d3
valentine-ochieng <ojvalentine14@gmail.com>
2023-01-03 23:14:01 +0300

    Registered the admin model

--- core/models.py
+++ core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Image(models.Model):
     image=models.ImageField(upload_to='images/')
-    # date = models.DateTimeField( auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now())
 
     class Meta:
         ordering=['-date']
 
     def __str__(self):
-        return str(self.date)
+        return str(self.image)
