c845347871a3a741fdbf85e912ca6d6e5049b87b
valentine-ochieng <ojvalentine14@gmail.com>
2023-01-03 22:12:52 +0300

    Registered the admin model

--- core/models.py
+++ core/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    ImageType = models.CharField(max_length=50, blank=True, null=True)
+    imageType = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return  str(self.imageType)
 
 class ImageFile(models.Model):
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    pic = models.ForeignKey(Image, on_delete=models.CASCADE)
     image=models.ImageField(upload_to='images/')
     date = models.DateTimeField( auto_now_add=True)
 
